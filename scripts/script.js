15069d8d901e4809be9dcf747da90371ff03eab0
mobin <mbynatrzade@gmail.com>
2022-10-25 13:25:56 +0330

    header section responsive

--- scripts/script.js
+++ scripts/script.js
@@ -14,3 +14,18 @@ header_menu_level_two.forEach(function (item) {
         item.querySelector('a').insertAdjacentHTML('beforeend', '<i class="fas fa-chevron-left"></i>');
     }
 })
+
+// --- responsive menu
+const responsive_menu_list = document.querySelectorAll('.responsive-menu__list ul li');
+responsive_menu_list.forEach(function (item) {
+    if (item.querySelector('ul') !== null) {
+        item.querySelector('a').insertAdjacentHTML('afterend', '<i class="fas fa-chevron-down"></i>');
+        const arrow = item.querySelector('.fa-chevron-down');
+        arrow.addEventListener('click', function (event) {
+            const target_el = event.target;
+            const ul_el = target_el.parentElement.querySelector('ul');
+            ul_el.classList.toggle('show');
+            target_el.classList.toggle('rotate');
+        })
+    }
+})
